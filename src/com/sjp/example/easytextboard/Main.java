660f1e3da89090ede1efcf608072562ada74975b
sjun8932 <sjun8932@gmail.com>
2021-01-21 15:13:10 +0900

    게시물 작성 시 번호가 자동으로 증가하도록 처리

--- src/com/sjp/example/easytextboard/Main.java
+++ src/com/sjp/example/easytextboard/Main.java
@@ -4,16 +4,19 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("명령어 ) ");
 
 		Scanner scanner = new Scanner(System.in);
 
+		int lastArticleId =0;
+		
 		while (true) {
+			System.out.printf("명령어) ");
 			String command = scanner.nextLine();
 
 			if (command.equals("article add")) {
 				System.out.println("== 게시물 등록 ==");
 				
+				int id = lastArticleId + 1;
 				String title;
 				String body;
 				
@@ -21,20 +24,23 @@ public class Main {
 				title = scanner.nextLine();
 				System.out.printf("내용 : ");
 				body = scanner.nextLine();
-				int id = 1;
 				
-				System.out.println("== 입력된 내용 ==");
-				System.out.println("번호 " + id);
-				System.out.println("제목: " + title);
-				System.out.println("내용: " + body);
+				lastArticleId = id;
 				
+				System.out.println("== 생성된 게시물 정보 ==");
+				System.out.printf("번호 : %d\n" , id);
+				System.out.printf("제목: %s\n" , title);
+				System.out.printf("내용: %s\n" , body);
 				
 			} else if (command.equals("article list")) {
 				System.out.println("== 게시물 리스트==");
+		
 			} else if (command.equals("system exit")) {
-				System.out.println("프로그램 종료");
+				System.out.println("==시스템 종료==");
 				break;
-			} else {
+			}
+			
+			else {
 				System.out.println("==존재하지 않는 명령어==");
 			}
 		}
