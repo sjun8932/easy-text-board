7ec12df036a3b73aa5fc2857581c7a4241f57ce2
SANGJUN PARK <sjun8932@gmail.com>
2021-03-12 15:50:17 +0900

    게시물 수정

--- src/com/sjp/example/easytextboard/App.java
+++ src/com/sjp/example/easytextboard/App.java
@@ -3,11 +3,11 @@ package com.sjp.example.easytextboard;
 import java.util.Scanner;
 
 public class App {
-	Article[] articles = new Article[3];
-	int lastArticleId = 0;
-	int articlesSize = 0;
+	private Article[] articles = new Article[3];
+	private int lastArticleId = 0;
+	private int articlesSize = 0;
 
-	int articlesSize() {
+	private int articlesSize() {
 		return articlesSize;
 	}
 
@@ -60,6 +60,15 @@ public class App {
 		return -1;
 	}
 
+	private void modify(int inputedId, String title, String body) {
+
+		Article article = getArticle(inputedId);
+		article.title = title;
+		article.body = body;
+
+	}
+	
+	// 가장 상위층 시작
 	public void run() {
 
 		Scanner sc = new Scanner(System.in);
@@ -123,6 +132,26 @@ public class App {
 				System.out.printf("번호 : %d\n", article.id);
 				System.out.printf("제목 : %s\n", article.title);
 				System.out.printf("내용 : %s\n", article.body);
+			} else if (command.startsWith("article modify ")) {
+				int inputedId = Integer.parseInt(command.split(" ")[2]);
+				System.out.println("== 게시물 수정 ==");
+
+				Article article = getArticle(inputedId);
+				
+				if (article == null) {
+					System.out.printf("%d번 게시물이 존재하지 않습니다.\n", inputedId);
+					continue;
+				}
+
+				System.out.printf("번호 : %d\n", article.id);
+				System.out.printf("제목 : ");
+				String title = sc.nextLine();
+				System.out.printf("내용 : ");
+				String body = sc.nextLine();
+
+				modify(inputedId, title, body);
+
+				System.out.printf("%d번 게시물이 수정되었습니다.\n", inputedId);
 			} else if (command.startsWith("article delete ")) {
 				int inputedId = Integer.parseInt(command.split(" ")[2]);
 				System.out.println("== 게시물 삭제 ==");
